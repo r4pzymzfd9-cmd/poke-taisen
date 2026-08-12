0a144b97556b9ea94943f0399d251d9150b8d65e
r4pzymzfd9-cmd <r4pzymzfd9@privaterelay.appleid.com>
2026-08-13 00:37:56 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="pc-scan-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(h=>h||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r;}).catch(()=>caches.match("./index.html"))))});
